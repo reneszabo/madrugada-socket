ca7c598f8ace035f9dd7dceb743b92ba4a4b6aaf
Rene Ramirez <rene.szabo@gmail.com>
2017-01-16 22:49:34 -0500

    Animate numbers and get data drom IO

--- server.js
+++ server.js
@@ -12,19 +12,37 @@ var io = require('socket.io')(http);
 var rn = require('random-number').generator({min:10000, max:99999, integer: true});
 var moment = require('moment');
 // --- ------------------------------------------------------------------------------------------------------------- ---
+// --- Global Variables
+// --- ------------------------------------------------------------------------------------------------------------- ---
+var data = {date: getDate(), followers: rn()};
+// --- ------------------------------------------------------------------------------------------------------------- ---
 // --- Express Settings
 // --- ------------------------------------------------------------------------------------------------------------- ---
 app.use(express.static(path.join(__dirname + '/public')));
 http.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function () {
     var addr = http.address();
-    console.log("Our server is listening at", addr.address + ":" + addr.port + ' - ' +  moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
+    console.log("Our server is listening at", addr.address + ":" + addr.port + ' - ' +  getDate());
 });
 // --- ------------------------------------------------------------------------------------------------------------- ---
 // --- Express Settings
 // --- ------------------------------------------------------------------------------------------------------------- ---
 io.on('connection', function (socket) {
-    console.log('a user connected - ' +  moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
+    console.log('a user connected - ' +  getDate());
+    updateTime();
     socket.on('disconnect', function () {
         console.log('user disconnected');
     });
+    socket.on('update-page', function () {
+        data = {date: getDate(), followers: rn()};
+        updateTime();
+    });
 });
+// --- ------------------------------------------------------------------------------------------------------------- ---
+// --- Global Functions
+// --- ------------------------------------------------------------------------------------------------------------- ---
+function updateTime() {
+    io.emit('onUpdateData', data);
+}
+function getDate() {
+    return moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+}
